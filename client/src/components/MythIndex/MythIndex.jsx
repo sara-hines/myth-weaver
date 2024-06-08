cdfb7e53c997e343c41f16a9cfe3e51b879b626a
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-06-08 16:54:34 -0600

    added in responsiveness to myth-index and changed some things on the boxes for the stories.

--- client/src/components/MythIndex/MythIndex.jsx
+++ client/src/components/MythIndex/MythIndex.jsx
@@ -58,7 +58,6 @@
 
 // export default MythIndex;
 
-
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_STORIES } from '../../utils/queries';
@@ -92,20 +91,24 @@ const MythIndex = () => {
                     {stories.map((story, index) => (
                         <Link to={`/story-index/${story._id}`} key={story._id}>
                             <div className={`story-container story story-${index + 1} heartbeat`}>
-                                <p className='story-title'>{story.title}</p>
-                                <p className='story-author'>Created by {story.author}</p>
-                                <p className='story-description'>{story.description}</p>
                                 <div className='story-image-wrapper'>
                                     <img className='story-image' src={story.imageUrl} alt={story.title} />
                                 </div>
-                                {story.reviews?.length === 0 || !story?.reviews ? (
-                                    <p>No ratings yet!</p>
-                                ) : (
-                                    <>
-                                        {renderStars(story.averageRating)}
-                                        <p>Rated {story.averageRating} stars on average by {story.ratingsCount} people.</p>
-                                    </>
-                                )}
+                                <div className="story-title-container">
+                                    <p className='story-title'>{story.title}</p>
+                                </div>
+                                <div className="story-info">
+                                    <p className='story-author'>Created by {story.author}</p>
+                                    <p className='story-description'>{story.description}</p>
+                                    {story.reviews?.length === 0 || !story?.reviews ? (
+                                        <p>No ratings yet!</p>
+                                    ) : (
+                                        <>
+                                            {renderStars(story.averageRating)}
+                                            <p>Rated {story.averageRating} stars on average by {story.ratingsCount} people.</p>
+                                        </>
+                                    )}
+                                </div>
                             </div>
                         </Link>
                     ))}
